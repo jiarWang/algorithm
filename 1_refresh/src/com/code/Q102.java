797d96d57a3645e5eef8fd200114c81987160e7a
jiarWang <2186000761@qq.com>
2021-02-12 22:21:22 +0800

    DFS实现二叉树层级遍历带层数

--- 1_refresh/src/com/code/Q102.java
+++ 1_refresh/src/com/code/Q102.java
@@ -11,13 +11,36 @@ import java.util.Queue;
  * 102. 二叉树的层序遍历
  *
  * 给你一个二叉树，请你返回其按 层序遍历 得到的节点值。 （即逐层地，从左到右访问所有节点）。
+ *
+ * 示例：
+ * 二叉树：[3,9,20,null,null,15,7],
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * 返回其层序遍历结果：
+ *
+ * [
+ *   [3],
+ *   [9,20],
+ *   [15,7]
+ * ]
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/binary-tree-level-order-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Q102 {
     public static void main(String[] args) {
 
     }
+
+    //BFS主要思路就是广度优先
     public static List<List<Integer>> levelOrder(TreeNode root) {
-        //主要思路就是广度优先
+
         List<List<Integer>> result = new ArrayList<>();
         if (root != null){
             int level = 0;
@@ -36,6 +59,16 @@ public class Q102 {
                 level ++;
             }
         }
+        return result;
+    }
+
+    //DFS 深度优先
+    public static List<List<Integer>> levelOrder(TreeNode node, int level, List<List<Integer>> result){
+        if (node == null) return result;
+        if (result.size() <= level) result.add(new ArrayList<>());
+        result.get(level).add(node.val);
+        levelOrder(node.left, level + 1, result);
+        levelOrder(node.right, level + 1, result);
         return result;
     }
 }
