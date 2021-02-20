ab6be76a5608f31ff4c175d6be807e4c2b856e29
jiarWang <2186000761@qq.com>
2021-02-20 22:58:37 +0800

    [1][Q121、Q122][买入股票最佳时机问题]

---
+++ 1_refresh/src/com/code/Q121.java
@@ -0,0 +1,38 @@
+package com.code;
+
+//给定一个数组 prices ，它的第 i 个元素 prices[i] 表示一支给定股票第 i 天的价格。
+//
+// 你只能选择 某一天 买入这只股票，并选择在 未来的某一个不同的日子 卖出该股票。设计一个算法来计算你所能获取的最大利润。
+//
+// 返回你可以从这笔交易中获取的最大利润。如果你不能获取任何利润，返回 0 。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：[7,1,5,3,6,4]
+//输出：5
+//解释：在第 2 天（股票价格 = 1）的时候买入，在第 5 天（股票价格 = 6）的时候卖出，最大利润 = 6-1 = 5 。
+//     注意利润不能是 7-1 = 6, 因为卖出价格需要大于买入价格；同时，你不能在买入前卖出股票。
+//
+public class Q121 {
+    public static void main(String[] args) {
+        System.out.println(maxProfit(new int[]{7, 1, 5, 3, 6, 4}));
+    }
+
+    public static int maxProfit(int[] prices) {
+        if (prices == null || prices.length <= 1) return 0;
+        //dp[i][j] => 第i天结束，是(否, j = 0否)持股，手上拥有的现金数
+        int[][] dp = new int[prices.length][2];
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for (int i = 1; i < prices.length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1] + prices[i]);
+            dp[i][1] = Math.max(-prices[i], dp[i - 1][1]);
+        }
+        return dp[prices.length - 1][0];
+    }
+
+
+}
